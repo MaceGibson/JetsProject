a34d6c884679a3c825f1fcfd40175f8f1f8e6d0a
MaceGibson <mace_gibson@yahoo.com>
2023-12-01 17:57:33 -0500

    initial commit

---
+++ src/com/skilldistillery/jets/AirField.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.jets;
+
+import java.util.ArrayList;
+
+public class AirField {
+	private ArrayList <Jet> jests;
+	
+}
---
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.jets;
+
+public class JetsApplication {
+	
+	private AirField airField;
+	
+	public static void main(String[] args) {
+
+	}
+}
+///takes user input and calls AirField's methods
